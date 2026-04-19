33d8e12bc66dd0f14513152a901e5a5e3ec29853
Adam Rackis <adamrackis@hotmail.com>
2026-04-18 20:19:49 -0500

    small tweak

--- src/components/ApplicationShell.tsx
+++ src/components/ApplicationShell.tsx
@@ -7,7 +7,7 @@ export const ApplicationShell: FC<PropsWithChildren> = (props) => {
     <main>
       <header className="fixed top-0 left-0 right-0 h-12 z-10 bg-blue-200">Header</header>
       <section className="fixed left-0 top-12 bottom-0 w-30 overflow-auto bg-amber-200">Sidebar</section>
-      <section className="max-w-[600px] mx-auto">
+      <section className="max-w-[600px] mt-12 pt-4 mx-auto">
         <div className="flex flex-col gap-2">
           <section className="min-h-[200px]">{children}</section>
           <footer className="bg-blue-300">Footer</footer>
