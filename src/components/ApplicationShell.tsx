05ecdea51ee6e9c7d04898ff527ae3f61d9017e5
Adam Rackis <adamrackis@hotmail.com>
2026-05-13 22:52:01 -0500

    Suspense works

--- src/components/ApplicationShell.tsx
+++ src/components/ApplicationShell.tsx
@@ -1,20 +1,20 @@
-import type { FC, PropsWithChildren } from "react";
+import { Suspense, type FC, type PropsWithChildren } from "react";
 import { SideBarContent } from "./SideBarContent";
 
 type ApplicationShellProps = {
-  HeaderContent?: FC<{ avatar: string }>;
+  HeaderContent?: FC<{ name: string; avatar: string }>;
   FooterContent: FC;
 };
 
 export const ApplicationShell: FC<PropsWithChildren<ApplicationShellProps>> = (props) => {
   const { children, HeaderContent, FooterContent } = props;
 
-  const avatar = "https://d193qjyckdxivp.cloudfront.net/avatar.jpg";
-
   return (
     <main className="h-screen">
       <header className="fixed top-0 left-0 right-0 h-12 z-10 bg-blue-200 flex items-center px-4 gap-4">
-        <span className="w-6 h-6">{HeaderContent ? <HeaderContent avatar={avatar} /> : null}</span>
+        <Suspense fallback={<span className="w-6 h-6 bg-gray-400 rounded-full"></span>}>
+          <UserHeaderMenu HeaderContent={HeaderContent} />
+        </Suspense>
         <span>Header</span>
       </header>
       <section className="fixed left-0 top-12 bottom-0 w-60 overflow-auto ">
@@ -31,3 +31,16 @@ export const ApplicationShell: FC<PropsWithChildren<ApplicationShellProps>> = (p
     </main>
   );
 };
+
+async function UserHeaderMenu(props: { HeaderContent?: FC<{ name: string; avatar: string }> }) {
+  if (!props.HeaderContent) {
+    return null;
+  }
+  const { HeaderContent } = props;
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const avatar = "https://d193qjyckdxivp.cloudfront.net/avatar.jpg";
+  const name = "Adam Rackis";
+
+  return <HeaderContent name={name} avatar={avatar} />;
+}
